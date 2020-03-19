dc1e0d4af8f2c39f1e3570a14119d592e2f22a15
DESKTOP-6VVMHFQ\HEGS <81766839@qq.com>
2020-03-19 15:16:21 +0800

    新建项目

---
+++ assets/js/test.js
@@ -0,0 +1,7 @@
+/**
+ * Created by HEGS on 2019/10/8.
+ */
+export function test () {
+  "use strict";
+  console.log('123456')
+}
---
+++ assets/js/test2.js
@@ -0,0 +1,5 @@
+/**
+ * Created by HEGS on 2019/10/8.
+ */
+import {test} from './test.js'
+test()
